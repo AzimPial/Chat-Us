db1e06001eded3d21e755e9a8a7b70284abbf5ff
Azim Pial <azimpial@example.com>
2025-11-28 17:45:12 +0600

    Fix: Clean repo state and update app features

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-
 import { getStorage } from "firebase/storage";
 
 // TODO: Replace the following with your app's Firebase project configuration
